3fdeffc295c42aa58727e6a5a2873417693276fc
kihBootCamp <kimberleyheuer@hotmail.com>
2021-03-19 05:51:08 -0500

    started some caching of files

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,9 @@ const FILES_TO_CACHE = [
   "/",
   "index.html",
   "index.js",
-  "styles.css" 
-    // "public/icons/icon-192x192.png",
-    // "public/icons/icon-512x512.png"
+  "styles.css",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png"
     //"/manifest.webmanifest"
 ]
 
